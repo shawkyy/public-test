b8aa5cd7520a1370225b29312ace9cfa77c9af56
Shawky <abdalla.ahmed@deliveryhero.com>
2022-06-17 19:44:04 +0200

    no message

--- Models.swift
+++ Models.swift
@@ -22,3 +22,51 @@ enum JSONFile: String {
   case pandoraTemplates = "pandora-templates"
   case talabatTemplates = "talabat-templates"
 }
+// Created on 01.04.22. Copyright © 2022 Delivery Hero. All rights reserved.
+
+import Foundation
+
+struct ComponentsModel: Decodable {
+  let components: [ComponentAPIModel]
+}
+
+enum JSONTestDecoderErrors: Error {
+  case fileNotFound(filename: String)
+}
+
+enum JSONFile: String {
+  // MARK: - Components files
+  case accessibilityComponents = "accessibility-components"
+  case homescreenComponents = "homescreen-components"
+  case pagersComponents = "pagers-components"
+  case rlpComponents = "rlp-components"
+  case talabatComponents = "talabat-components"
+
+  // MARK: - Templates files
+  case pandoraTemplates = "pandora-templates"
+  case talabatTemplates = "talabat-templates"
+}
+// Created on 01.04.22. Copyright © 2022 Delivery Hero. All rights reserved.
+
+import Foundation
+
+struct ComponentsModel: Decodable {
+  let components: [ComponentAPIModel]
+}
+
+enum JSONTestDecoderErrors: Error {
+  case fileNotFound(filename: String)
+}
+
+enum JSONFile: String {
+  // MARK: - Components files
+  case accessibilityComponents = "accessibility-components"
+  case homescreenComponents = "homescreen-components"
+  case pagersComponents = "pagers-components"
+  case rlpComponents = "rlp-components"
+  case talabatComponents = "talabat-components"
+
+  // MARK: - Templates files
+  case pandoraTemplates = "pandora-templates"
+  case talabatTemplates = "talabat-templates"
+}
